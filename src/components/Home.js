59d9d72fe0ffaf9c5439fbf1d499a873b5de741e
Anouarelaoud <anouaraitelaoud@gmail.com>
2023-11-03 15:53:37 +0100

    add Button component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
+import Button from "./Button";
 
 const Home = () => {
   const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
@@ -36,6 +37,9 @@ const Home = () => {
           />
         ))}
       </Grid>
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" />
+      )}
     </>
   );
 };
