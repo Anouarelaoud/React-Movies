e5e11081bcc15039c503f87e86fb0a5f917e47f8
Anouarelaoud <anouaraitelaoud@gmail.com>
2023-11-03 12:15:00 +0100

    add Thumb component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useHomeFetch } from "../hooks/useHomeFetch";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
-// import NoImage from "../images/no_image.jpg";
+import NoImage from "../images/no_image.jpg";
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
+import Thumb from "./Thumb";
 
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
@@ -13,14 +14,23 @@ const Home = () => {
     <>
       {state.results[0] ? (
         <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[11].backdrop_path}`}
-          title={state.results[11].original_title}
-          text={state.results[11].overview}
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
         />
       ) : null}
       <Grid header="popular movies">
         {state.results.map((movie) => (
-          <div key={movie.id}> {movie.title}</div>
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+          />
         ))}
       </Grid>
     </>
