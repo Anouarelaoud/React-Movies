adf6f69e578f5833e8f538615a8f12df6204e99a
Anouarelaoud <anouaraitelaoud@gmail.com>
2023-11-08 12:49:27 +0100

    add Actor component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
-import BreadCrumb from "./BreadCrumb";
-import Grid from "./Grid";
-import Spinner from "./Spinner";
+
 import { useMovieFetch } from "../hooks/useMovieFetch";
 import NoImage from "../images/no_image.jpg";
+
+import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+import Grid from "./Grid";
+import Actor from "./Actor";
 
 const Movie = () => {
   const { movieId } = useParams();
@@ -20,6 +24,25 @@ const Movie = () => {
     <>
       <BreadCrumb movieTitle={movie.original_title} />
       <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
